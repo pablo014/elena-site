056d56adb66e218b103fd4b8a7526af062dc46a7
pablo014 <angelopablo014@gmail.com>
2023-08-05 15:07:20 -0700

    feat(setup): install vue full page

--- elena-site/src/App.vue
+++ elena-site/src/App.vue
@@ -3,15 +3,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <full-page>
+    <div class="fp-section">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <div class="fp-section">
+      <HelloWorld msg="Vite + Vue" />
+    </div>
+  </full-page>
 </template>
 
 <style scoped>
--- elena-site/src/main.ts
+++ elena-site/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'vue-fullpage.js/dist/style.css'
+import VueFullPage from 'vue-fullpage.js'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(VueFullPage)
+    .mount('#app')
