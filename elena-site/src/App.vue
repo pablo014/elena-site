4cdd96021f22385fbea5599390e01ce6ab4a8728
pablo014 <angelopablo014@gmail.com>
2023-08-06 19:27:32 -0700

    feat(navigation): add nav bar

--- elena-site/src/App.vue
+++ elena-site/src/App.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
 import { VueFlip } from 'vue-flip';
+const options = {
+  licenseKey: 'YOUR_KEY_HERE',
+      menu: '#menu',
+      anchors: ['home', 'about_me'],
+};
 </script>
 
 <template>
-  <full-page>
-    <div class="fp-section front">
+  <div class="w-screen absolute z-20 h-12 p-2 bg-gray-700 text-lg sm:text-3xl">
+    <a class="p-2 m-2" href="#home">Home</a>
+    <a class="p-2 m-2" href="#about_me">About Me</a>
+  </div>
+  <full-page :options="options">
+    <div class="fp-section front" data-anchor="home">
       <div class="bg-gradient-to-b from-black to-green-900 absolute top-0 bottom-0 left-0 right-0 opacity-60 z-0"></div>
       <div class="z-10 absolute top-40 bottom-10 sm:left-10 left-4 sm:right-10 right-4 flex flex-col justify-between">
         <div>
@@ -24,7 +33,7 @@ import { VueFlip } from 'vue-flip';
         </div>
       </div>
     </div>
-    <div class="fp-section">
+    <div class="fp-section" data-anchor="about_me">
       <div class="bg-gradient-to-b from-black to-green-900 absolute top-0 bottom-0 left-0 right-0 opacity-60"></div>
       <div class="z-10">test</div>
     </div>
